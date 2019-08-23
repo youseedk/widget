1a6822c43670d3caf9c8dc984684c6820d4fd01a
MichaeL Grandorf <mgb@grandorf.dk>
2019-08-23 15:09:55 +0200

    wip

--- content.js
+++ content.js
@@ -1,89 +1,104 @@
 // content.js
 
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.message === "clicked_browser_action") {
-
+*/
       /******************* BEGIN ******************/
 
+      //Create templates
       const templateHead = `<title>&#x1F527; ${document.title}</title>`
       const templateBody = `
-          <aside id="widget">
-             <button type="button" id="mgbTest">Edit mode</button>
-             <button type="button" id="htmlTest">Html validator</button>
+          <aside id="widget" class="animated slideInDown">
+             <button type="button" id="featureHtmlValidator">Html validator</button>
+             
+             <div class="ys-toggle-switch">
+                <input type="checkbox" id="featureEdit" class="ys-toggle-switch__control" />
+                <label for="featureEditText" class="ys-toggle-switch__label">Grid</label>
+            </div>
           </aside>
           <iframe id="content" src="#" frameborder="0" />
         `
-
+      //Append templates to dom
       document.head.innerHTML = templateHead;
       document.body.innerHTML = templateBody;
 
+      //Set iframe source
       const iframe = document.querySelector("#content");
       iframe.setAttribute('src', location.href);
 
+      //when iframe is loaded
       iframe.onload = function () {
-        document.querySelector('#mgbTest').addEventListener('click', () => {
-          iframe.contentDocument.designMode = "on";
+
+        //Html validator button
+        document.querySelector('#featureHtmlValidator').addEventListener('click', () => {
+          htmlValidator();
         });
 
+        //Edit mode button
+        document.querySelector('#featureEditText').addEventListener('click', () => {
+          iframe.contentDocument.designMode = (iframe.contentDocument.designMode == "on") ? 'off' : 'on';
+        });
 
-        document.querySelector('#htmlTest').addEventListener('click', () => {
-          //Inspired from from http://stackoverflow.com/questions/10377840/making-finding-html5-validator-bookmarklet
+     
 
-          function domToString(document_root) {
-            var html = '',
-              node = document_root.firstChild;
-            while (node) {
-              switch (node.nodeType) {
-                case Node.ELEMENT_NODE:
-                  html += node.outerHTML;
-                  break;
-                case Node.TEXT_NODE:
-                  html += node.nodeValue;
-                  break;
-                case Node.CDATA_SECTION_NODE:
-                  html += '<![CDATA[' + node.nodeValue + ']]>';
-                  break;
-                case Node.COMMENT_NODE:
-                  html += '<!--' + node.nodeValue + '-->';
-                  break;
-                case Node.DOCUMENT_TYPE_NODE:
-                  // (X)HTML documents are identified by public identifiers
-                  html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-                  break;
-              }
-              node = node.nextSibling;
+
+
+      //Inspired from from http://stackoverflow.com/questions/10377840/making-finding-html5-validator-bookmarklet
+      function htmlValidator() {
+        function domToString(document_root) {
+          let html = '',
+            node = document_root.firstChild;
+          while (node) {
+            switch (node.nodeType) {
+              case Node.ELEMENT_NODE:
+                html += node.outerHTML;
+                break;
+              case Node.TEXT_NODE:
+                html += node.nodeValue;
+                break;
+              case Node.CDATA_SECTION_NODE:
+                html += '<![CDATA[' + node.nodeValue + ']]>';
+                break;
+              case Node.COMMENT_NODE:
+                html += '<!--' + node.nodeValue + '-->';
+                break;
+              case Node.DOCUMENT_TYPE_NODE:
+                // (X)HTML documents are identified by public identifiers
+                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+                break;
             }
-            return html;
+            node = node.nextSibling;
           }
+          return html;
+        }
 
-          (function () {
-            var html_to_validate = domToString(iframe.contentDocument);
-            function append(key, value) {
-              var input = document.createElement('textarea');
-              input.name = key;
-              input.value = value;
-              form.appendChild(input);
-            }
-            var form = document.createElement('form');
-            form.method = 'POST';
-            form.action = '//validator.w3.org/check';
-            form.enctype = 'multipart/form-data';         // Required for this validator
-            form.target = '_blank';                       // Open in new tab
-            append('fragment', html_to_validate);         // <-- Code to validate
-            append('doctype', 'HTML5');                   // Validate as HTML 5
-            append('group', '0');
-            document.body.appendChild(form);
-            form.submit();
-            form.parentNode.removeChild(form);
-          })();
-        });
-      };
-
-
-
+        (function () {
+          const html_to_validate = domToString(iframe.contentDocument);
+          function append(key, value) {
+            const input = document.createElement('textarea');
+            input.name = key;
+            input.value = value;
+            form.appendChild(input);
+          }
+          var form = document.createElement('form');
+          form.method = 'POST';
+          form.action = '//validator.w3.org/check';
+          form.enctype = 'multipart/form-data';         // Required for this validator
+          form.target = '_blank';                       // Open in new tab
+          append('fragment', html_to_validate);         // <-- Code to validate
+          append('doctype', 'HTML5');                   // Validate as HTML 5
+          append('group', '0');
+          document.body.appendChild(form);
+          form.submit();
+          form.parentNode.removeChild(form);
+        })();
+      }
+    };
 
       /******************* END ******************/
+      /*
     }
   }
 ); 
+*/
